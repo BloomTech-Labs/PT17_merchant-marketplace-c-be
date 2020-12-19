66289adda5ef9c5bc03de6237fadac3d19bdf001
Nicholas Mullen <nwmullen@gmail.com>
2020-12-18 19:45:23 -0500

    fix linter formatting errors

--- data/seeds/05_categories.js
+++ data/seeds/05_categories.js
@@ -1,7 +1,7 @@
 exports.seed = function (knex) {
   // Inserts seed entries
   return knex('category').insert([
-    {  category_name: 'Consumer Goods' },
-    {  category_name: 'Billionaire Toys' },
+    { category_name: 'Consumer Goods' },
+    { category_name: 'Billionaire Toys' },
   ]);
 };
