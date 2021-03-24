b91dd04128904e08cbd67db82da24fa7f655d153
Jonathan Portillo <54336208+jonathan-portillo@users.noreply.github.com>
2021-03-23 22:08:58 -0400

    post method to create a buyer profile

--- api/profile/buyerRouter.js
+++ api/profile/buyerRouter.js
@@ -13,4 +13,29 @@ router.get('/', authRequired, function (req, res) {
     });
 });
 
+router.post('/', authRequired, async (req, res) => {
+  const profile = req.body;
+  if (profile) {
+    const id = profile.id || 0;
+    try {
+      await Model.findById('buyer_profile', id).then(async (pf) => {
+        if (pf == undefined) {
+          await Model.create('buyer_profile', profile).then((profile) =>
+            res
+              .status(200)
+              .json({ message: 'profile created', profile: profile[0] })
+          );
+        } else {
+          res.status(400).json({ message: 'profile already exists' });
+        }
+      });
+    } catch (e) {
+      console.error(e);
+      res.status(500).json({ message: e.message });
+    }
+  } else {
+    res.status(404).json({ message: 'Profile missing' });
+  }
+});
+
 module.exports = router;
