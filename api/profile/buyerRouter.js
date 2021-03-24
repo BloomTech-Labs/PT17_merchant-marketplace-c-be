8899884fb2ae9ddbf8887970c1d1bca85519c6ec
Jonathan Portillo <54336208+jonathan-portillo@users.noreply.github.com>
2021-03-23 21:48:49 -0400

    get method created for buyer router

--- api/profile/buyerRouter.js
+++ api/profile/buyerRouter.js
@@ -1,4 +1,16 @@
 const express = require('express');
+const authRequired = require('../middleware/authRequired');
 const router = express.Router();
+const Model = require('../globalModel');
+
+router.get('/', authRequired, function (req, res) {
+  Model.findAll('buyer_profile')
+    .then((profiles) => {
+      res.status(200).json(profiles);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err.message });
+    });
+});
 
 module.exports = router;
