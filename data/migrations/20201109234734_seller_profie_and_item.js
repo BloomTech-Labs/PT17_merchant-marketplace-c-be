c80226b906718487b5e8d0ca713e14d435b560c9
lex-marie790 <lexi.marie790@gmail.com>
2021-03-29 20:04:36 -0700

    added added defaultTo(false) to pickup and delivery

--- data/migrations/20201109234734_seller_profie_and_item.js
+++ data/migrations/20201109234734_seller_profie_and_item.js
@@ -29,8 +29,8 @@ exports.up = function (knex) {
         .inTable('seller_profile')
         .onDelete('CASCADE')
         .onUpdate('CASCADE');
-      tb.boolean('pickup');
-      tb.boolean('delivery');
+      tb.boolean('pickup').defaultTo(false);
+      tb.boolean('delivery').defaultTo(false);
     })
     .createTable('photo', (tb) => {
       tb.increments();
