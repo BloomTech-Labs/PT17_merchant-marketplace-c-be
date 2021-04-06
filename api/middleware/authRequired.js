ff21d136a572d26a54667408ed6fca16cb801aa2
Jonathan Portillo <54336208+jonathan-portillo@users.noreply.github.com>
2021-04-05 21:12:25 -0400

    deleted commented out code

--- api/middleware/authRequired.js
+++ api/middleware/authRequired.js
@@ -19,31 +19,6 @@ const makeProfileObj = (claims) => {
  * if the token is not present or fails validation. If the token is valid its
  * contents are attached to req.profile
  */
-// const authRequired = async (req, res, next) => {
-//   try {
-//     const authHeader = req.headers.authorization || '';
-//     const match = authHeader.match(/Bearer (.+)/);
-
-//     if (!match) throw new Error('Missing idToken');
-
-//     const idToken = match[1];
-//     oktaJwtVerifier
-//       .verifyAccessToken(idToken, oktaVerifierConfig.expectedAudience)
-//       .then(async (data) => {
-//         const jwtUserObj = makeProfileObj(data.claims);
-//         const sellerProfile = await Model.findOrCreate('seller_profile', jwtUserObj);
-//         const buyerProfile = await Model.findOrCreate('buyer_profile', jwtUserObj)
-//         if (sellerProfile) {
-//           req.profile = sellerProfile;
-//         } else {
-//           throw new Error('Unable to process idToken');
-//         }
-//         next();
-//       });
-//   } catch (err) {
-//     next(createError(401, err.message));
-//   }
-// };
 
 const authRequired = async (req, res, next) => {
   try {
