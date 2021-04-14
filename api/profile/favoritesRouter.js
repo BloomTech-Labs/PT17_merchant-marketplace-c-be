d88f797ff56d0fee6050d71618b1fc3c5146fd2f
aaron wesolek <maxwes13@gmail.com>
2021-04-13 20:10:53 -0500

    fixed favorites model

--- api/profile/favoritesRouter.js
+++ api/profile/favoritesRouter.js
@@ -3,16 +3,19 @@ const authRequired = require('../middleware/authRequired');
 const router = express.Router();
 const Model = require('../globalModel');
 
-router.get('/:id/favorites', authRequired, function (req, res) {
-  const id = String(req.params.id);
-  Model.favorites('favorite_category', id)
-    .then((favorites) => {
-      res.status(200).json(favorites);
-    })
-    .catch((err) => {
-      res.status(500).json({ error: err.message });
-    });
-});
+router.get(
+  '/:id/favorites',
+  /* authRequired, */ function (req, res) {
+    const id = String(req.params.id);
+    Model.favorites(id)
+      .then((favorites) => {
+        res.status(200).json(favorites);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  }
+);
 
 router.post('/favorites', authRequired, function (req, res) {
   const profile = req.profile;
